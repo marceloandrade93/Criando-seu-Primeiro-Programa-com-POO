0673a85249a6e71000fa1df06915f15385779ccc
marcelo <andradeswork@gmail.com>
2024-04-08 10:14:25 -0300

    fim da aula de herança multipla

--- 3_heranca_simples.py
+++ 3_heranca_simples.py
@@ -5,7 +5,7 @@ class Veiculo:
         self.numero_rodas = numero_rodas
 
     def ligar_motor(self):
-        print("Ligando o motoro...")
+        print("Ligando o motor...")
 
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
