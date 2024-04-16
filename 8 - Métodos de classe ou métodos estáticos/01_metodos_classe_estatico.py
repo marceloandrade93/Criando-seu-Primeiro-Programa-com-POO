bda14e98443c57e3fc76428ca6c7f4019fea1642
marcelo <andradeswork@gmail.com>
2024-04-16 18:17:08 -0300

    Ajuste para retornar a maioridade de forma mais amigável.

--- 8 - Métodos de classe ou métodos estáticos/01_metodos_classe_estatico.py
+++ 8 - Métodos de classe ou métodos estáticos/01_metodos_classe_estatico.py
@@ -3,17 +3,20 @@ class Pessoa:
         self.nome = nome
         self.idade = idade
     
+    # para métodos de fábrica - que retornam instâncias dessa classe
     @classmethod
     def criar_de_nascimento(cls, ano, mes, dia, nome):
         idade = 2024 - ano
         return cls(nome, idade)
     
+    # para métodos estáticos, que apenas fazem sentido estar na classe
     @staticmethod
     def e_maior_idade(idade):
-        return idade >= 18
+        return f"{'É' if (idade >= 18) else 'Não é'} maior de idade!"
+    
 
 p = Pessoa.criar_de_nascimento(1991, 2, 15, "Mateus")
 print(p.nome, p.idade)
 
-print(Pessoa.e_maior_idade(18))
 print(Pessoa.e_maior_idade(10))
+print(Pessoa.e_maior_idade(18))
