9262006865dcb2ac0963423bed31c08a17782503
marcelo <andradeswork@gmail.com>
2024-04-16 18:51:23 -0300

    Aula sobre classe abstrata. Houve mudança no abstractproperty. Na documentação é um método em desuso.

---
+++ 9 - Classes abstratas/01_classe_abstrata.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+class ControleRemoto(ABC):
+    @abstractmethod
+    def ligar(self):
+        pass
+
+    @abstractmethod
+    def desligar(self):
+        pass
+
+    @property
+    @abstractmethod  # na aula usava o @abstractproperty, mas na documentação foi recomendado usar desta forma.
+    def marca(self):
+        pass
+
+class ControleTV(ControleRemoto):
+    def ligar(self):
+        print("Ligando a TV...")
+        print("Ligada!")
+
+    def desligar(self):
+        print("Desligando a TV...")
+        print("Desligada!")
+
+    @property
+    def marca(self):
+        return "LG"
+
+class ControleArCondicionado(ControleRemoto):
+    def ligar(self):
+        print("Ligando o Ar Condicionado...")
+        print("Ligado!")
+
+    def desligar(self):
+        print("Desligando o Ar Condicionado...")
+        print("Desligado!")
+
+    @property
+    def marca(self):
+        return "Philco"
+    
+controle = ControleTV()
+controle.ligar()
+controle.desligar()
+print(controle.marca)
+
+controle = ControleArCondicionado()
+controle.ligar()
+controle.desligar()
+print(controle.marca)
